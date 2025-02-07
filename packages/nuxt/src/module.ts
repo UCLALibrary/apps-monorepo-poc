338f3a2093612d2a645e11989f7485603863efcb
pghorpade <pghorpade@library.ucla.edu>
2025-02-06 17:09:06 -0800

    feat: create nuxt module to enable autoimports

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,19 +1,27 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
-
+import { addComponent, defineNuxtModule, createResolver, useLogger } from '@nuxt/kit'
+import * as VueComponentLibrary from "vue-component-library"
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: '@stellar/nuxt',
+    configKey: 'stellar',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
-
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    const logger = useLogger("@stellar/nuxt")
+    // const resolver = createResolver(import.meta.url)
+    _nuxt.options.css.push("vue-component-library/style.css")
+    Object.keys(VueComponentLibrary)
+      .forEach((component) => {
+        console.log(component)
+        addComponent({
+          filePath: "vue-component-library",
+          name: component,
+          export: component,
+        })
+      })
   },
 })
